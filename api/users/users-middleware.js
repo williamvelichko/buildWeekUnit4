5dfbf3f0c4919a2ca23452b7f70cd55ddf6a2544
williamvelichko <williamvelichko2003@gmail.com>
2022-02-28 15:32:24 -0800

    added middleware

---
+++ api/users/users-middleware.js
@@ -0,0 +1,27 @@
+const model = require("./users-model");
+const db = require("./../data/db-config");
+const jwt = require("jsonwebtoken");
+
+const validateRegister = async (req, res, next) => {
+  const username = req.body.username;
+  if (!username || !req.body.password) {
+    res.status(401).json({ message: "username and password required" });
+  } else {
+    next();
+  }
+};
+
+const usernameExists = async (req, res, next) => {
+  const username = req.body.username;
+  const result = await model.getByFilter({ username });
+  if (result) {
+    res.status(401).json({ message: "username is already taken" });
+  } else {
+    next();
+  }
+};
+
+module.exports = {
+  validateRegister,
+  usernameExists,
+};
--- api/users/users-router.js
+++ api/users/users-router.js
@@ -2,20 +2,26 @@ const router = require("express").Router();
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const model = require("./users-model");
+const mid = require("./users-middleware");
 
-router.post("/register", (req, res, next) => {
-  const { username, password } = req.body;
+router.post(
+  "/register",
+  mid.validateRegister,
+  mid.usernameExists,
+  (req, res, next) => {
+    const { username, password } = req.body;
 
-  const hash = bcrypt.hashSync(password, 8);
-  model
-    .addUser({ username, password: hash })
-    .then((user) => {
-      res.status(200).json(user);
-    })
-    .catch((err) => {
-      res.json(err.message);
-    });
-});
+    const hash = bcrypt.hashSync(password, 8);
+    model
+      .addUser({ username, password: hash })
+      .then((user) => {
+        res.status(200).json(user);
+      })
+      .catch((err) => {
+        res.json(err.message);
+      });
+  }
+);
 router.post("/login", (req, res, next) => {});
 router.get("/logout", (req, res, next) => {});
 
